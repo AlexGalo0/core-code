57d942ce93640aa40a1c507c78877faffbc32791
Alex Galo <alexgalo246@gmail.com>
2022-02-06 14:07:58 -0600

    ejercicios week 3 martes

--- week3/2-Tuesday/1-OrderPlease.js
+++ week3/2-Tuesday/1-OrderPlease.js
@@ -1,11 +1,25 @@
 function order(words){
-    //Convertir string a Array
-    
-    
-    //Recorremos cada palabra del array, al encontrar un typeof num , identificacmos que numero es
 
-    
+    if(words=="") return false ;  //Si la palabra esta vacia
+
+    var array = words.split('') ;  //Dividir la palabra en arreglos
+   
+    var objetoTemporal = {} ; 
+    //Obtencion de orden
+    for(var element of array) {
+        var number = getNumber(element) ; //Obtendremos el numero de la RegEx
+        objetoTemporal[number] = element ;  //En Objeto Temporal, en la posicion numero colocaremos el elemento
+    }
+    //Concatenacion
+    return Object.values(objetoTemporal).join(' ') ; 
+
+}
+
+function getNumber(s) {
+    console.log(s);
+    //El metodo match retorna si se encuentra la expresion regular
+    return s.match(/\d/)[0] ; 
+}
 
 
-
-  }
+order("Dan1el S5o Al1x") ; 
---
+++ week3/2-Tuesday/3-SimplePigLatin.js
@@ -0,0 +1,19 @@
+function pigIt(str) {
+    var newArr=[] ; 
+    var stringArray=str.split(" ") ;  //Divide en palabras
+
+    stringArray.forEach(x => { //Recorremos todo el array
+        let wordArr= x.split("") ;   //Dividimos toda la palabra
+        wordArr.push(wordArr[0],'ay') ,wordArr.shift()  //el Shift elimina el primer elemento del array, dentro de la palabra, haremos un push al final, del primer caracter + 'ay'
+        if(x=== "!" || x=== "?" || x=== ".") { //Signos de Exclamacion
+            newArr.push(x) ; 
+        } else {
+            newArr.push(wordArr.join("")) ; 
+        }
+    });
+
+    return newArr.join(" ") ; 
+
+}
+
+pigIt("hola que tal")

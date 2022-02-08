5995b2bcb43c0915df56cf8c04e55d43c1d8a0d2
Alex Galo <alexgalo246@gmail.com>
2022-02-08 10:43:36 -0600

    ejercicio completado parentesis

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Core Code Bootcamp
 
-## Week 1
+## Software Development From Scrach
+
+### Week 1
 [Se adjuntan los ejercicios de la semana 1.](week1)
 
-## Week 2 
+### Week 2 
 [Se adjuntan los ejercicios de la semana 2.](week2)
 
-
-## Week 3
+### Week 3
 [Se adjuntan los ejercicios de la semana 3.](week3)
 
-## Week 4
+### Week 4
 [Se adjuntan los ejercicios de la semana 4.](week4)
--- week3/3-Wednesday/1-Parentesis.js
+++ week3/3-Wednesday/1-Parentesis.js
@@ -1,36 +1,25 @@
 function validParentheses(parens) {
-   var longitud = parens.length ; 
-  
-    //Recorrer Stringqq
-    var parentesisIncompletos=0 ; 
-   
-     if(parens.charAt(0)===")" || parens.charAt(parens.length -1 )=== "(") return false ; 
-   
-     for(let i = 0 ; i<=longitud;i++) {
-
-       if(parens.charAt(i) ==="(") {
-        parentesisIncompletos+=1 ; 
-        console.log("Parentesis ( se hallo") ; 
-       } else {
-        if(parens.charAt(i) ===")") {
-          parentesisIncompletos-=1 ; 
-          console.log("Parentesis ) se hallo") ; 
-         }
-       }
-       }
+  var longitud = parens.length ; 
+   //Recorrer Stringqq
+   var parentesisIncompletos=0 ; 
+    if(parens.charAt(0)===")" || parens.charAt(parens.length -1 )=== "(") return false ;  
+    for(let i = 0 ; i<=longitud;i++) {
+      if(parens.charAt(i) ==="(") {
+       parentesisIncompletos+=1 ; 
+      } else {
+       if(parens.charAt(i) ===")") {
+         parentesisIncompletos-=1 ; 
+        }
+        if(parentesisIncompletos<0) {
+          return false ; 
+        }
+      }
+      }
+     return(parentesisIncompletos==0)
       
-       console.log(parentesisIncompletos) ; 
-
-       if(parentesisIncompletos==0) {
-        return true ; 
-       } else {
-         return false ; 
-       }
-
-       
-
    
-  }
-   
+      
 
+
+ }
    validParentheses("())(()")

f89b568c955d4c28d7744b8c84226a86959ca621
Alex Galo <alexgalo246@gmail.com>
2022-03-04 16:54:46 -0600

    adding foldArray exercise

--- week3/1-Monday/3-MorseCode1.js
+++ week3/1-Monday/3-MorseCode1.js
@@ -1,5 +1,1 @@
 /* https://www.codewars.com/kata/54b724efac3d5402db00065e */
-
-decodeMorse = function(morseCode){
-    //your code here
-  }
--- week3/4-Thursday/Exercises/1-FoldArray.js
+++ week3/4-Thursday/Exercises/1-FoldArray.js
@@ -1,2 +1,17 @@
 /* https://www.codewars.com/kata/57ea70aa5500adfe8a000110 */
 
+function foldArray(array,runs) {
+    if(runs<1) return array ;
+
+    const folded = [...array] ;
+    let runsCopy = runs ; 
+
+
+    while(runsCopy>0) {
+        folded.map((num,numIndex,NumArr)=>{
+            numIndex+1===NumArr.length ? num : NumArr[numIndex]= num+ NumArr.pop()
+        })
+        runsCopy--
+    }
+    return folded ; 
+}

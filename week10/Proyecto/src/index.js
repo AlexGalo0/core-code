c6db7d6caa06514c7889a7e3bb48dc52870344cc
Alex Galo <alexgalo246@gmail.com>
2022-03-21 23:03:46 -0600

    finish the backend of the proyect

--- week10/Proyecto/src/index.js
+++ week10/Proyecto/src/index.js
@@ -1,11 +1,14 @@
+const { urlencoded } = require('express');
 const express = require('express');
+const { RequestHandler } = require('./handlers/todos');
 const {initializeDB} = require('./lib/db')
+
 
 const App = express () ; 
 
 App.use(express.json()) //Todo lo que venga del cliente pasara por aca,Y a todo lo pondra en JSON
-
-
+App.use(express.urlencoded({extended:false}))
+App.use(RequestHandler)
 
 App.listen(3000,()=>{
   console.log("Estoy escuchando ");
